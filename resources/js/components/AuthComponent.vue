7f0da546025ac6e42d2eedbef20dde2aa38ec702
Evgeniy Ralchenko <evgeniy.ralchenko@cyberia.studio>
2022-03-13 19:23:39 +0700

    fix veri mutch

--- resources/js/components/AuthComponent.vue
+++ resources/js/components/AuthComponent.vue
@@ -1,9 +1,22 @@
 <template>
-<div>
-    <button v-on:click="login()">Войти</button>
-    <br>
-    <button v-on:click="register()">Зарегистрироваться</button>
-</div>
+    <div class="content">
+        <div class="content__top">
+            <div class="container">
+                <p class="game__name__title">
+                    название игры
+                </p>
+                <p class="game__name__description">
+                    Помогите котенку стать инженером-энергетиком!
+                </p>
+                <div class="button__container">
+                    <button class="go" v-on:click="login()">Войти</button>
+                    <br>
+                    <button v-on:click="register()">Зарегистрироваться</button>
+                </div>
+                <img src="" alt="а котека то нет">
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -37,6 +50,18 @@ export default {
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.container {
+    max-width: 1660px;
+}
 
+.content__top {
+    height: 840px;
+    /*background-image: url("../assets/Frame.png");*/
+    background-color: #4C3375;
+}
+
+.content {
+
+}
 </style>
---
+++ resources/js/components/MainMenuComponent.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <div class="content">
+        <div class="content__top">
+            <div class="container">
+                <p class="content__top__title">
+                    название игры
+                </p>
+                <p class="content__top__description">
+                    Помогите котенку стать инженером-энергетиком!
+                </p>
+                <div class="content__button">
+                    <button class="content__button__register">
+                        регистрация
+                    </button>
+                    <button class="content__button__login">
+                        вход
+                    </button>
+                </div>
+                <img src="resources/img/main_cat.svg" alt="а котека то нет">
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "MainMenuComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
