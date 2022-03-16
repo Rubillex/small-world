7c4473ce6d1d5e28f29fc8696122afd9582f5253
Evgeniy Ralchenko <evgeniy.ralchenko@cyberia.studio>
2022-03-16 07:53:47 +0700

    started making Auth-component

--- resources/js/components/AuthComponent.vue
+++ resources/js/components/AuthComponent.vue
@@ -1,9 +1,48 @@
 <template>
-<div>
-    <button v-on:click="login()">Войти</button>
-    <br>
-    <button v-on:click="register()">Зарегистрироваться</button>
-</div>
+    <div class="content">
+        <div class="content__top">
+            <div class="container">
+                <p class="content__top__title">
+                    название игры
+                </p>
+                <p class="content__top__description">
+                    Помогите котенку стать инженером-энергетиком!
+                </p>
+                <div class="content__button">
+                    <button class="content__button__login" v-on:click="login()">Войти</button>
+                    <br>
+                    <button class="content__button__register" v-on:click="register()">Зарегистрироваться</button>
+                </div>
+                <img class="content__top__img" src="/images/main_cat.svg" alt="а котека то нет">
+            </div>
+        </div>
+        <div class="content__mid">
+            <h2 class="content__mid__title">
+                Привет, студент!
+            </h2>
+            <p class="content__mid__description">
+                Если ты здесь, значит из тебя в будущем может получится отличный инженер энергетик. Возможно, ты ещё
+                не так много знаешь об этой науке, но наши котята-инженеры хотят немного поднять твой уровень.
+            </p>
+            <p class="content__mid__description">
+                Сейчас ты получишь доступ к секретной системе обучения студентов-энергетиков. Эта система воспитала
+                уже не одно поколение настоящих профессионалов. А чтобы тебе не было скучно, мы дадим тебе пушистого
+                товарища.
+            </p>
+            <p class="content__mid__description">
+                Вместе веселее!
+            </p>
+        </div>
+        <div class="content__bottom">
+
+        </div>
+        <div class="content__bottom">
+
+        </div>
+        <div class="content__bottom">
+
+        </div>
+    </div>
 </template>
 
 <script>
